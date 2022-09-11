165f08398f00f18a26d31449f62bf285ec49ae2e
kim young been <51738790+dudqls611@users.noreply.github.com>
2022-09-12 03:39:14 +0900

    Add files via upload

--- 4_starbucks_project/js/main.js
+++ 4_starbucks_project/js/main.js
@@ -15,3 +15,27 @@ SearchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   SearchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+// _.throttle : lodash cdn 검색하여 가져옴
+// _.throttle(함수, 시간(밀리세컨드 단위))
+// gsap cdn 검색하여 가져옴
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // 배지를 숨겨줌
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // 배지 보여줌
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+  
+}, 300));
